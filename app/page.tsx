aac66463aea848926e57e4df849d3def2706514d
Marcelo Fortunato <mp.fortunato88@gmail.com>
2023-10-09 19:27:47 +0000

    feat(home): Update font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-24 gap-5">
-      <h1>nauts.digital</h1>
-      <p>design & development</p>
+    <main className="flex h-screen w-full flex-col items-center justify-center px-24 gap-3">
+      <h1 className="text-7xl">nauts.digital</h1>
+      <p className="text-2xl">design & development</p>
     </main>
   );
 }
