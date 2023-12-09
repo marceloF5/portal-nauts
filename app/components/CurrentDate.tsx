026d6e2b44f5b37a2e32c8316e1cd545531bc6c6
Marcelo Fortunato <mp.fortunato88@gmail.com>
2023-12-09 10:39:42 +0000

    Update CurrentDate.tsx

--- app/components/CurrentDate.tsx
+++ app/components/CurrentDate.tsx
@@ -2,14 +2,14 @@
 import { useState, useEffect } from 'react'
 
 export default function CurrentDate() {
-  const [currentDate, setCurrentDate] = useState(null)
+  const [currentDate, setCurrentDate] = useState('')
 
   setTimeout(()=> {
     const currentDay = `${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
     const seconds = new Date().getSeconds() < 10 ? `0${new Date().getSeconds()}` : new Date().getSeconds()
     const currentTime = `${new Date().getHours()}:${new Date().getMinutes()}:${seconds}`
-    setCurrentDate(`${currentDay} - ${currentTime}`)
+    setCurrentDate(`, ${currentDay} - ${currentTime}`)
   }, 1000)
   
-  return <p>Lisbon, {currentDate}</p>
+  return <p>Lisbon{currentDate}</p>
 }
