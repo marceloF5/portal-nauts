9407cb81a824ecb837ea480139d639b9586be702
Marcelo Fortunato <mp.fortunato88@gmail.com>
2023-12-09 10:36:50 +0000

    Add Current Date component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import CurrentDate from './components/CurrentDate'
+
 export default function Home() {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center px-24 gap-3">
-      <h1 className="text-7xl">nauts.digital</h1>
+      <div className="flex">
+        <h1 className="text-7xl transition duration-1000 opacity-100 ">
+          nauts
+        </h1>
+        <h1 className="text-7xl transition duration-1000 opacity-50 delay-1000">
+          .digital
+        </h1>
+      </div>
       <p className="text-2xl">design & development</p>
+
+      <CurrentDate />
     </main>
   );
 }
