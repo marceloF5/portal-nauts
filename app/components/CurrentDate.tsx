7697e00a3e1c16048f95dff3864bad675c043674
Marcelo Fortunato <mp.fortunato88@gmail.com>
2023-12-10 09:18:10 +0000

    Update current date

--- app/components/CurrentDate.tsx
+++ app/components/CurrentDate.tsx
@@ -1,15 +1,35 @@
-'use client'
-import { useState, useEffect } from 'react'
+"use client";
+import { useState, useEffect } from "react";
 
 export default function CurrentDate() {
-  const [currentDate, setCurrentDate] = useState('')
+  const [currentDate, setCurrentDate] = useState("");
 
-  setTimeout(()=> {
-    const currentDay = `${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
-    const seconds = new Date().getSeconds() < 10 ? `0${new Date().getSeconds()}` : new Date().getSeconds()
-    const currentTime = `${new Date().getHours()}:${new Date().getMinutes()}:${seconds}`
-    setCurrentDate(`, ${currentDay} - ${currentTime}`)
-  }, 1000)
-  
-  return <p>Lisbon{currentDate}</p>
+  setTimeout(() => {
+    const month =
+      new Date().getMonth() < 10
+        ? `0${new Date().getMonth()}`
+        : new Date().getMonth();
+    const day =
+      new Date().getDate() < 10
+        ? `0${new Date().getDate()}`
+        : new Date().getDate();
+    const currentDay = `${day}/${month}/${new Date().getFullYear()}`;
+
+    const hours =
+      new Date().getHours() < 10
+        ? `0${new Date().getHours()}`
+        : new Date().getHours();
+    const minutes =
+      new Date().getMinutes() < 10
+        ? `0${new Date().getMinutes()}`
+        : new Date().getMinutes();
+    const seconds =
+      new Date().getSeconds() < 10
+        ? `0${new Date().getSeconds()}`
+        : new Date().getSeconds();
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    setCurrentDate(`, ${currentDay} - ${currentTime}`);
+  }, 1000);
+
+  return <p>Lisbon{currentDate}</p>;
 }
